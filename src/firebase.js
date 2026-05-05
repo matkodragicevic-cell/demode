8be475fba05d71273f5708b24b0b8d42fd4b99f6
matkodragicevic-cell <matko.dragicevic@gmail.com>
2026-05-05 07:41:26 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
+  apiKey: "AIzaSyB2L4MZfrZbgV_XZdhpmMmh3_EJfUUfrHM",
   authDomain: "demode-evidencija-gostiju.firebaseapp.com",
   projectId: "demode-evidencija-gostiju",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  storageBucket: "demode-evidencija-gostiju.firebasestorage.app",
+  messagingSenderId: "575181335280",
+  appId: "1:575181335280:web:53530ab9ad29c5b655f035"
 };
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
