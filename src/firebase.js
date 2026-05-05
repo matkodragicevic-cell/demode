68f1428505dc14a74619012efe5576b8916f4d7b
matkodragicevic-cell <matko.dragicevic@gmail.com>
2026-05-05 07:32:44 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, signInAnonymously } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2L4MZfrZbgV_XZdhpmMmh3_EJfUUfrHM",
+  apiKey: "OVDJE_TVOJ_API_KEY",
   authDomain: "demode-evidencija-gostiju.firebaseapp.com",
   projectId: "demode-evidencija-gostiju",
-  storageBucket: "demode-evidencija-gostiju.firebasestorage.app",
-  messagingSenderId: "575181335280",
-  appId: "1:575181335280:web:53530ab9ad29c5b655f035"
+  storageBucket: "OVDJE_TVOJ_STORAGE",
+  messagingSenderId: "OVDJE_TVOJ_SENDER_ID",
+  appId: "OVDJE_TVOJ_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+signInAnonymously(auth).catch((error) => {
+  console.error("Anonimna prijava neuspješna:", error);
+});
